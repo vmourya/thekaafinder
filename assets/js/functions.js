a8cba4d8f701fb92cc4a95c1afcf70ea32bdf7bd
unknown <vijay.mourya@wks172.nsi.lab>
2015-07-22 14:12:08 +0530

    First Push to Git of Thekaafinder 220715

---
+++ assets/js/functions.js
@@ -0,0 +1,84 @@
+function initialize() {
+  var mapOptions = {
+    zoom: 17
+  };
+  
+ /* Showing current location start */ 
+ if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = new google.maps.LatLng(position.coords.latitude,
+                                       position.coords.longitude);
+      var infowindow = new google.maps.InfoWindow({
+        map: map,
+        position: pos,
+        content: 'You are here<br /><span class="find-from-here">Search Arround.</span>'
+      });
+      map.setCenter(pos);
+    }, function() {
+      handleNoGeolocation(true);
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleNoGeolocation(false);
+  } 
+ /* Showing current location end */ 
+  var map = new google.maps.Map(document.getElementById('map-canvas'),
+    mapOptions);
+
+  var input =(document.getElementById('s'));
+
+  var types = document.getElementById('type-selector');
+  //map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  //map.controls[google.maps.ControlPosition.TOP_LEFT].push(types);
+
+  var autocomplete = new google.maps.places.Autocomplete(input);
+  autocomplete.bindTo('bounds', map);
+
+  var infowindow = new google.maps.InfoWindow();
+  var marker = new google.maps.Marker({
+    map: map,
+    anchorPoint: new google.maps.Point(0, -29)
+  });
+
+  google.maps.event.addListener(autocomplete, 'place_changed', function() {
+    infowindow.close();
+    marker.setVisible(false);
+    var place = autocomplete.getPlace();
+    if (!place.geometry) {
+      window.alert("Autocomplete's returned place contains no geometry");
+      return;
+    }
+	alert(place.geometry.location);
+    // If the place has a geometry, then present it on a map.
+    if (place.geometry.viewport) {
+      map.fitBounds(place.geometry.viewport);
+    } else {
+      map.setCenter(place.geometry.location);
+      map.setZoom(17);  // Why 17? Because it looks good.
+    }
+    marker.setIcon(({
+      url: place.icon, // SETTING MARKER ICON
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(35, 35)
+    }));
+    marker.setPosition(place.geometry.location);
+    marker.setVisible(true);
+
+    var address = '';
+    if (place.address_components) {
+      address = [
+        (place.address_components[0] && place.address_components[0].short_name || ''),
+        (place.address_components[1] && place.address_components[1].short_name || ''),
+        (place.address_components[2] && place.address_components[2].short_name || '')
+      ].join(' ');
+    }
+
+    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+    infowindow.open(map, marker);
+  });
+
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+		<title>Find where you are ?</title>
+		<link href='assets/css/styles.css' rel='stylesheet'/>
+		<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places"></script>
+		<script src='assets/js/functions.js'></script>
+	</head>
+	<body>
+		<header>
+			<input id="s" name='s' class="controls" type="text"
+        placeholder="Enter a location">
+		</header>
+		<section>
+			<div id="map-canvas"></div>
+		</section>
+		<section>
+			
+		</section>
+	</body>
+</html>
