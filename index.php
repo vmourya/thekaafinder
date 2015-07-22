97ef6a7ec7e35d7c008f1f21080b15bc5c06ad67
Vijay <vijay.mourya@wks172.nsi.lab>
2015-07-22 18:22:56 +0530

    UI with Listing

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
-<html>
+<html> 
 	<head>
 		<title>Find where you are ?</title>
 		<link href='assets/css/styles.css' rel='stylesheet'/>
+		<script src="https://code.jquery.com/jquery-1.11.3.min.js" ></script>
 		<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places"></script>
 		<script src='assets/js/functions.js'></script>
 	</head>
 	<body>
+	<div class='wrapper-main'>
 		<header>
 			<input id="s" name='s' class="controls" type="text"
         placeholder="Enter a location">
@@ -14,7 +16,58 @@
 			<div id="map-canvas"></div>
 		</section>
 		<section>
-			
+			<h1>Listing</h1>
+			<ul class="search-listing">
+				<li>
+					<p>
+						<span>
+							<a href='javascript:void(0);' class="location-item" ><strong>Abc Def.</strong> ipsum is simply</a>
+						</span>
+						<span>About 10 K.M</span>
+					</p>
+				</li>
+				<li>
+					<p>
+						<span>
+							<a href='javascript:void(0);' class="location-item" ><strong>Abc Def.</strong> ipsum is simply</a>
+						</span>
+						<span>About 10 K.M</span>
+					</p>
+				</li>
+				<li>
+					<p>
+						<span>
+							<a href='javascript:void(0);' class="location-item" ><strong>Abc Def.</strong> ipsum is simply</a>
+						</span>
+						<span>About 10 K.M</span>
+					</p>
+				</li>
+				<li>
+					<p>
+						<span>
+							<a href='javascript:void(0);'class="location-item" ><strong>Abc Def.</strong> ipsum is simply</a>
+						</span>
+						<span>About 10 K.M</span>
+					</p>
+				</li>
+				<li>
+					<p>
+						<span>
+							<a href='javascript:void(0);' class="location-item" ><strong>Abc Def.</strong> ipsum is simply</a>
+						</span>
+						<span>About 10 K.M</span>
+					</p>
+				</li>
+				<li>
+					<p>
+						<span>
+							<a href='javascript:void(0);' class="location-item" ><strong>Abc Def.</strong> ipsum is simply</a>
+						</span>
+						<span>About 10 K.M</span>
+					</p>
+				</li>
+			</ul>
 		</section>
+		</div>
 	</body>
 </html>
